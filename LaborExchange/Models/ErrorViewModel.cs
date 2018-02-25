04111f8549fc5a55333bcf6fc8cb07557cc382f9
Serhii Yakhin <Neamiades@gmail.com>
2018-02-25 23:46:55 +0200

    Add project files.

---
+++ LaborExchange/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LaborExchange.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
